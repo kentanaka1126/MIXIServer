bc27a0080e6000086d5bcb420dd65719397fd3fe
kentanaka1126 <kentanaka1126@outlook.com>
2024-10-13 04:57:16 -0700

    workspace design

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ dotenv.config();
 require("./config/passport"); // Import passport configuration
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(
   session({
     secret: "yourSecretKey",
